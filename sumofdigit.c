5c5f191dc7d3114621758e768b7b4e0a78396a93
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-03-07 16:43:28 +0530

    sumofdigit.c
    good

---
+++ sumofdigit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i,n,r,sum=0;
+    clrscr();
+    
+    printf("Enter N:");
+    scanf("%d",&n);
+    
+    while(n!=0)
+    {
+     r=n%10;
+     sum=sum+r;
+     n=n/10;
+    }
+    printf("\nThe Sum is %d",sum);
+}
